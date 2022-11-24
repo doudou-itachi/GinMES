ff53c179779e1593dec4ba57e055c19213dad52f
zhangmaolin <zhangamaolin@troila.com>
2022-11-24 17:35:41 +0800

    ADD JWT Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNfdmFsaWQiOjEsImNyZWF0ZV90aW1lIjoiMjAyMi0xMS0yNFQxNzoyOTozNCswODowMCIsInVwZGF0ZV90aW1lIjoiMjAyMi0xMS0yNFQxNzoyOTozNiswODowMCIsInVzZXJuYW1lIjoiYWRtaW4iLCJwYXNzd29yZCI6IjEyMzQ1NiIsImV4cCI6MTY2OTI4MjU3NS4xMzMwNTgsImlzcyI6Inh4In0.hHP_Uie0TxK5xXvUYPDoGrtGgitgdGxlw6_JIeoY7zA

---
+++ utils/jwtutil.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"GinMES/models"
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go/v4"
+	"time"
+)
+
+var MySecret = []byte("密钥")
+
+// 创建 Token
+func GenToken(user models.Users) (string, error) {
+	claim := models.CustomClaims{
+		user,
+		jwt.StandardClaims{
+			ExpiresAt: jwt.At(time.Now().Add(time.Minute * 5)), //5分钟后过期
+			Issuer:    "xx",                                    //签发人
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+	return token.SignedString(MySecret)
+}
+
+// 解析 token
+func ParseToken(tokenStr string) (*models.CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &models.CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return MySecret, nil
+	})
+	if err != nil {
+		fmt.Println(" token parse err:", err)
+		return nil, err
+	}
+	if claims, ok := token.Claims.(*models.CustomClaims); ok && token.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("invalid token")
+}
+
+// 刷新 Token
+func RefreshToken(tokenStr string) (string, error) {
+	jwt.TimeFunc = func() time.Time {
+		return time.Unix(0, 0)
+	}
+
+	token, err := jwt.ParseWithClaims(tokenStr, &models.CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return MySecret, nil
+	})
+	if err != nil {
+		return "", err
+	}
+	if claims, ok := token.Claims.(*models.CustomClaims); ok && token.Valid {
+		jwt.TimeFunc = time.Now
+		claims.StandardClaims.ExpiresAt = jwt.At(time.Now().Add(time.Minute * 10))
+		return GenToken(claims.Users)
+	}
+	return "", errors.New("Cloudn't handle this token")
+}
